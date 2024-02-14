297bdd2faabffd4125f2bca5df6fd4d568b0929f
lap-a-toon <lap-a-toon@yandex.ru>
2024-02-14 23:17:35 +0500

    добавил STDIN Доп. проверка. Если скрипт запускается без параметров, используется STDIN

--- exercise-02.php
+++ exercise-02.php
@@ -3,22 +3,34 @@
 
 $minInput = 2; // Определяем минимальное количество параметров
 $maxInput = 2; // Определяем максимальное количество параметров
-
-if(($argc-1)<$minInput){
-    fwrite(STDERR, "Вы указали недостаточно аргументов\n");
-    exit;
-}elseif(($argc-1)>$maxInput){
-    fwrite(STDERR, "Вы указали слишком много аргументов, обработаем только первые два\n");
+$numbers = []; // Создаём массив для чисел
+if($argc > 1){
+    if(($argc-1)<$minInput){
+        fwrite(STDERR, "Вы указали недостаточно аргументов\n");
+        exit;
+    }elseif(($argc-1)>$maxInput){
+        fwrite(STDERR, "Вы указали слишком много аргументов, обработаем только первые два\n");
+    }
+    // Заполняем массив числами
+    for($i=1; $i <= $maxInput; $i++){
+        $numbers[] = $argv[$i];
+    }
+}else{
+    // Заполняем массив числами из STDIN
+    for($i=0; $i < $maxInput; $i++){
+        echo "Введите ". ($i+1) . " число: ";
+        $numbers[] = trim(fgets(STDIN));
+    }
 }
 
 // дальше без оверкодинга
-if(!is_numeric($argv[1]) || !is_numeric($argv[2])){
+if(!is_numeric($numbers[0]) || !is_numeric($numbers[1])){
     fwrite(STDERR, "Все параметры должны быть числами\n");
     exit;
 }
-if($argv[2]==0){
+if($numbers[1]==0){
     fwrite(STDERR, "Делить на 0 нельзя\n");
     exit;
 }
 
-echo "$argv[1] / $argv[2] = ". ($argv[1]/$argv[2]);
+echo "$numbers[0] / $numbers[1] = ". ($numbers[0]/$numbers[1]);
